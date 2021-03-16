f0451ea7a2f7ae92470767ed908c17eeb861a975
joseph-tg <josephgatimu111@gmail.com>
2021-03-16 23:56:15 +0300

    removed read more on large screen

--- javascript/main.js
+++ javascript/main.js
@@ -27,73 +27,73 @@ function myFunction() {
 
 // Read more and See less syntax
 $(document).on('click','#more' , function(){
-    $('.more-info').addClass('show');
+    $('.info').addClass('show');
     $('.less').addClass('show');
     $('.more').addClass('hide');
-    $('.more-info').removeClass('hide');
+    $('.info').removeClass('hide');
     $('.less').removeClass('hide');
     $('.more').removeClass('show');
 });
 
 $(document).on('click','#less' , function(){
-    $('.more-info').addClass('hide');
+    $('.info').addClass('hide');
     $('.more').addClass('show');
     $('.less').addClass('hide');
-    $('.more-info').removeClass('show');
+    $('.info').removeClass('show');
     $('.less').removeClass('show');
     $('.more').removeClass('hide');
 });
 // Case II
 $(document).on('click','#more1' , function(){
-    $('.more-info1').addClass('show');
+    $('.info1').addClass('show');
     $('.less1').addClass('show');
     $('.more1').addClass('hide');
-    $('.more-info1').removeClass('hide');
+    $('.info1').removeClass('hide');
     $('.less1').removeClass('hide');
     $('.more1').removeClass('show');
 });
 
 $(document).on('click','#less1' , function(){
-    $('.more-info1').addClass('hide');
+    $('.info1').addClass('hide');
     $('.more1').addClass('show');
     $('.less1').addClass('hide');
-    $('.more-info1').removeClass('show');
+    $('.info1').removeClass('show');
     $('.less1').removeClass('show');
     $('.more1').removeClass('hide');
 });
 // Case III
 $(document).on('click','#more2' , function(){
-    $('.more-info2').addClass('show');
+    $('.info2').addClass('show');
     $('.less2').addClass('show');
     $('.more2').addClass('hide');
-    $('.more-info2').removeClass('hide');
+    $('.info2').removeClass('hide');
     $('.less2').removeClass('hide');
     $('.more2').removeClass('show');
 });
 
 $(document).on('click','#less2' , function(){
-    $('.more-info2').addClass('hide');
+    $('.info2').addClass('hide');
     $('.more2').addClass('show');
     $('.less2').addClass('hide');
-    $('.more-info2').removeClass('show');
+    $('.info2').removeClass('show');
     $('.less2').removeClass('show');
     $('.more2').removeClass('hide');
 });
 // Case IV
 $(document).on('click','#more3' , function(){
-    $('.more-info3').addClass('show');
+    $('.info3').addClass('show');
     $('.less3').addClass('show');
     $('.more3').addClass('hide');
-    $('.more-info3').removeClass('hide');
+    $('.info3').removeClass('hide');
     $('.less3').removeClass('hide');
     $('.more3').removeClass('show');
 });
 
 $(document).on('click','#less3' , function(){
-    $('.more-info3').addClass('hide');
+    $('.info3').addClass('hide');
     $('.more3').addClass('show');
     $('.less3').addClass('hide');
-    $('.more-info3').removeClass('show');
+    $('.info3').removeClass('show');
     $('.less3').removeClass('show');
     $('.more3').removeClass('hide');
 });
